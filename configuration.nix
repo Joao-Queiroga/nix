50e9f27e1960c2658cceed1a2459f67c68140fad
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-09-17 09:50:58 -0300

    added polkit configuration

--- configuration.nix
+++ configuration.nix
@@ -24,11 +24,33 @@
 		};
 	};
 
+	security.polkit.enable = true;
+	
+	systemd = {
+		user.services.polkit-gnome-authentication-agent-1 = {
+			description = "polkit-gnome-authentication-agent-1";
+			wantedBy = [ "graphical-session.target" ];
+			wants = [ "graphical-session.target" ];
+			after = [ "graphical-session.target" ];
+			serviceConfig = {
+				Type = "simple";
+				ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+				Restart = "on-failure";
+				RestartSec = 1;
+				TimeoutStopSec = 10;
+			};
+		};
+	};
+
 	nix.settings = {
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
 		experimental-features = [ "nix-command" "flakes" ];
 	};
+
+	nixpkgs.config.permittedInsecurePackages = [
+		"electron-12.2.3"
+	];
 
 	# networking.hostName = "nixos"; # Define your hostname.
 	# Pick only one of the below networking options.
