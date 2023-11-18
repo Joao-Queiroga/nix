3c27be553aa53abbd4ff245db6b2ee0e9f94d4a2
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-11-18 17:49:45 -0300

    changed hostname

--- flake.nix
+++ flake.nix
@@ -1,27 +1,33 @@
 {
   description = "My system flake";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		hyprland.url = "github:hyprwm/Hyprland";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
 
   outputs = { self, nixpkgs, ... } @ inputs:
-	let
-		system = "x86_64-linux";
+    let
+      system = "x86_64-linux";
 
-		pkgs = import nixpkgs {
-			inherit system;
+      pkgs = import nixpkgs {
+        inherit system;
 
-			config = { allowUnfree = true; };
-		};
-	in
-	{
-  nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-    specialArgs = { inherit system inputs; };
-    modules = [
-      ./configuration.nix
-    ];
-  };
-};
+        config = { allowUnfree = true; };
+
+      };
+      createConfiguration = hostname: nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit system inputs; };
+        modules = [
+          { networking.hostName = hostname; }
+          ./configuration.nix
+        ];
+      };
+    in
+    {
+      nixosConfigurations = {
+        tux = createConfiguration "tux";
+        tuxnote = createConfiguration "tuxnote";
+      };
+    };
 }
