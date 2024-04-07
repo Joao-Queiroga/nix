26c0c304ef17db69f27bf90a2699d5c185aa8b00
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-04-07 18:51:57 -0300

    add partition to tux

--- tux-config.nix
+++ tux-config.nix
@@ -4,42 +4,43 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.supportedFilesystems = [ "ntfs" ];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/558bc3be-4d67-481b-ae8d-fd334b7e95ac";
-      fsType = "btrfs";
-      options = [ "subvol=@" "compress=zstd:7" "space_cache=v2" ];
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/558bc3be-4d67-481b-ae8d-fd334b7e95ac";
+    fsType = "btrfs";
+    options = [ "subvol=@" "compress=zstd:7" "space_cache=v2" ];
+  };
 
-  fileSystems."/var" =
-    {
-      device = "/dev/disk/by-uuid/558bc3be-4d67-481b-ae8d-fd334b7e95ac";
-      fsType = "btrfs";
-      options = [ "subvol=@var" "compress=zstd:7" "space_cache=v2" ];
-    };
+  fileSystems."/var" = {
+    device = "/dev/disk/by-uuid/558bc3be-4d67-481b-ae8d-fd334b7e95ac";
+    fsType = "btrfs";
+    options = [ "subvol=@var" "compress=zstd:7" "space_cache=v2" ];
+  };
 
-  fileSystems."/home" =
-    {
-      device = "/dev/disk/by-uuid/9933da03-c31f-4fc9-bbf2-986d55d82d9a";
-      fsType = "btrfs";
-      options = [ "subvol=@home" "compress=zstd:7" "space_cache=v2" ];
-    };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/9933da03-c31f-4fc9-bbf2-986d55d82d9a";
+    fsType = "btrfs";
+    options = [ "subvol=@home" "compress=zstd:7" "space_cache=v2" ];
+  };
 
-  fileSystems."/boot/efi" =
-    {
-      device = "/dev/disk/by-uuid/B6F2-55CF";
-      fsType = "vfat";
-    };
+  fileSystems."/mnt/hd" = {
+    device = "/dev/disk/by-uuid/CC9859CE9859B822";
+    fsType = "ntfs-3g";
+    options = [ "rw" ];
+  };
+
+  fileSystems."/boot/efi" = {
+    device = "/dev/disk/by-uuid/B6F2-55CF";
+    fsType = "vfat";
+  };
 
   swapDevices = [ ];
 
@@ -51,5 +52,6 @@
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
